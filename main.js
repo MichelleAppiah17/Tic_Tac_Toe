c16fde9f69f6911d7769df140603d462e56545bc
Michelle Appiah <michappiah67@gmail.com>
2023-06-14 00:40:15 +0000

    checking for a Winner

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const inputs = document.querySelectorAll(".player_form input");
 const playBtn = document.querySelector(".play_btn");
 let playerX = document.querySelector("#player-X");
 let playerO = document.querySelector("#player-O");
+let message = document.querySelector("#message");
 const squares = document.querySelectorAll(".square");
 const restartBtn = document.querySelector(".restart_btn");
 
@@ -73,8 +74,51 @@ AIplayerBtn.addEventListener("click", function() {
     });   
 })
 
+function isBoardFull(){
+ for(let i = 0; i < squares.length; i ++){
+  if(squares[i].textContent == ""){
+    return false;
+  }
+ }
+ return true;
+};
+
+function checkWinner() {
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+    if (squares[a].textContent && 
+      squares[a].textContent === squares[b].textContent &&
+       squares[a].textContent === squares[c].textContent
+       ) {
+        
+      const winner = squares[a].textContent;
+      message.textContent = `Congratulations, ${winner} wins!`;
+      return squares[a].textContent;
+    }
+  }
+
+  if (isBoardFull()) {
+    message.textContent = "It's a tie!";
+    return 'tie';
+  }
+  return null;
+};
+
+
 let currentPlayer = 'X';
 let currentPlayerDisplay = document.querySelector("#current_Player");
+
 squares.forEach(square => {
   square.addEventListener('click', function() {
     if (!square.disabled) {
@@ -88,8 +132,18 @@ squares.forEach(square => {
         currentPlayerDisplay.textContent = "Player X's turn";
       }
       square.disabled = true;
-      return;
+
+      const winner = checkWinner();
+      if (winner !== null) {
+        currentPlayerDisplay.textContent = "";
+      }else if (isBoardFull()) {
+        message.textContent = "It's a tie!";
+        currentPlayerDisplay = "";
+      }else{
+        currentPlayerDisplay.textContent = `Player ${currentPlayer}'s turn`;
+      }
     }
+    
   });
 });
 
